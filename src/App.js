87d059d09851165d8d7e5e02620eac1bd66fbf68
Hissamkhan1 <hissamk19@gmail.com>
2024-06-29 14:09:06 +0500

    counter implemneted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [counter, setCounter]= useState(15)
 
-export default App
+  const addValue = (value) => {
+    // console.log("clicked", counter);
+    //  counter = counter + 1
+    if (counter < 20){
+    setCounter(counter + 1)
+  }else{
+    return
+  }
+  }
+  const removeValue = () => {
+  // counter = counter - 1
+    // console.log(counter);
+    if(counter > 1){
+    setCounter(counter - 1)
+  }
+  }
+
+  return(
+    <div>
+      <h1>Use-State</h1>
+      <h1>Counter_Value:{counter}</h1>
+      <button onClick={addValue}>Add Value</button>
+      <br/>
+      <button onClick={removeValue}>Remove Value</button>
+      </div>
+
+    )
+  }
+  export default App;
+  
